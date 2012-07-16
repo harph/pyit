7957f7669af751b7ec4481033f1a6d0edccffa48
Harrington Joseph <h.josephv@gmail.com>
2012-07-15 23:39:31 -0700

    Added 'fake' svg test. Needs a way to test the svg source

--- tests.py
+++ tests.py
@@ -4,22 +4,6 @@ import unittest
 import random
 import pyit
 
-"""
-Dummy versions:
-smaller x smaller
-smaller x exact
-exact x smaller
-exact x exact
-bigger x bigger
-bigger x exact
-exact x bigger
-smaller x bigger
-bigger x smaller
-
-Dummy sizes:
-500 x 200
-"""
-
 TEST_FOLDER = 'test'
 TEST_SIZES = (
     (100, 100), # small square
@@ -38,9 +22,11 @@ class TestPyIT(unittest.TestCase):
     def setUp(self):
         self.file_names = [
             f_name for f_name in os.listdir(TEST_FOLDER)
-            if f_name != '.DS_Store' and not os.path.isdir(os.path.join(TEST_FOLDER, f_name))
+            if f_name != '.DS_Store' and not os.path.isdir(
+                os.path.join(TEST_FOLDER, f_name)
+            )
         ]
-
+    
     def test_resize_and_crop(self):
         for file_name in self.file_names:
             file_path = os.path.join(TEST_FOLDER, file_name)
@@ -50,20 +36,19 @@ class TestPyIT(unittest.TestCase):
                 new_width, new_height = new_image.size
                 self.assertEqual(width, new_width)
                 self.assertEqual(height, new_height)
-                # new_image.show()
 
     def get_random_image(self):
         file_name = random.choice(self.file_names)
         file_path = os.path.join(TEST_FOLDER, file_name)
         image = Image.open(file_path)
         return image
-
+    
     def test_black_and_white(self):
         image = self.get_random_image()
         new_image = pyit.black_and_white(image)
         for pixel in new_image.getdata():
             self.assertTrue(pixel == 0 or pixel == 255)
-
+        
     def test_grayscale(self):
         file_name = random.choice(self.file_names)
         file_path = os.path.join(TEST_FOLDER, file_name)
@@ -71,6 +56,15 @@ class TestPyIT(unittest.TestCase):
         new_image = pyit.grayscale(image)
         for pixel in new_image.getdata():
             self.assertTrue(pixel <= 255)
+
+    def test_svg(self):
+        #TODO: Improve this test. Find a way to validate the svg source
+        image = self.get_random_image()
+        svg_source = pyit.svg_source(image)
+        svg_file = open('foo.svg', 'w')
+        svg_file.write(svg_source)
+        svg_file.close()
+        
 
 if __name__ == '__main__':
     unittest.main()
