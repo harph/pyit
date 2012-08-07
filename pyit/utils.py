52416ccbd4e53d6f45bfdc985a2ebfc5d839e199
Harrington Joseph <h.josephv@gmail.com>
2012-08-06 17:23:55 -0700

    Added function get_color_similarity for the utils module

--- pyit/utils.py
+++ pyit/utils.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import math
 
 
 def get_color_tuple(color):
@@ -53,3 +54,39 @@ def get_web_color(color):
     if len(color_tuple) == 3:
         return '#%02x%02x%02x' % color_tuple
     return 'rgba%s' % str(color_tuple)
+
+
+def _get_color_distance(color1, color2):
+    red1, green1, blue1 = get_color_tuple(color1)[:3]
+    red2, green2, blue2 = get_color_tuple(color2)[:3]
+    distance = math.sqrt(
+        math.pow(red1 - red2, 2) +
+        math.pow(green1 - green2, 2) +
+        math.pow(blue1 - blue2, 2)
+    )
+    return distance
+
+
+def _get_biggest_color_distance():
+    color1 = "#000000"
+    color2 = "#FFFFFF"
+    return _get_color_distance(color1, color2)
+
+
+def get_color_similarity(color1, color2):
+    """
+    Return a value from 0.0 to 1.0 that expresses how similar
+    is `color1` to `color2` based on the rgb dimentions distance
+    between them.
+    :param color1: Color in the following formats:
+        - rgb: rgb tuple, i.e.: (255, 255, 255)
+        - rgba: rgba tuple, i.e.: (255, 255, 255, 0.5)
+        - hex color: haxdeximal code format, i.e.: #00ffcc or #0fc
+    :param color2: Color in the following formats:
+        - rgb: rgb tuple, i.e.: (255, 255, 255)
+        - rgba: rgba tuple, i.e.: (255, 255, 255, 0.5)
+        - hex color: haxdeximal code format, i.e.: #00ffcc or #0fc
+    """
+    distance = _get_color_distance(color1, color2)
+    biggest_distance = _get_biggest_color_distance()
+    return 1.0 - float(distance / biggest_distance)
